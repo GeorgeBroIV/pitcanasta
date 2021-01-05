af9330d3d1917a5d77b83ce56117a7d49e515d62
George Brotherston <George@StonePlayground.com>
2021-01-05 15:48:00 -0500

    Add database migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,15 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'username',
+        'firstname',
+        'lastname',
+        'displayname',
         'email',
+        'avatar',
         'password',
+        'active',
+        'notes',
     ];
 
     /**
@@ -40,4 +46,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    
+    /* Custom User Methods
+     *  https://stackoverflow.com/questions/32437384/laravel-custom-user-specific-functions
+     */
+    
+    /**
+     * Usage: Auth()->user()->isVerified() (boolean)
+     *
+     * @return boolean
+     */
+    public function isVerified()
+    {
+        $verified = false;
+        if(in_array($this->attributes['role'], $this->rolesVerified)) {
+            $verified = true;
+        }
+        return $verified;
+    }
+    
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,25 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            // This information is only for the WebApp user info
+            // Social OAuth info are in other tables
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('displayname')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar')->nullable();
             $table->string('password');
+            $table->boolean('active')->default(1);
+            $table->text('notes')->nullable();
+            $table->dateTime('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('profilegame')->nullable();
+            $table->foreignId('updated_by')->nullable();
+            $table->foreignId('deleted_by')->nullable();
+            $table->softDeletes();
         });
     }
 
