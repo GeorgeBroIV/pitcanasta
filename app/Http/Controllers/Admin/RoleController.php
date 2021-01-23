b2c1f159251f58e92df13d650f628e4f0237bf6f
George Brotherston <George@StonePlayground.com>
2021-01-23 01:59:51 -0500

    Test Session Expiration
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -97,7 +97,7 @@ class RoleController extends Controller
         }
         
         $role->updated_by = $userId;
-dd($role, Auth()->user()->hasRole('Admin'));
+dd($request->protect, $role->protect);
         $role->save();
         // Once the model is updated, redirect the user to see the list of all Roles
         return redirect()->route('roles.index');
