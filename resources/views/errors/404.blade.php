536fb222ac0f84789e9c68f6c805524b647b3a8f
George Brotherston <George@StonePlayground.com>
2021-01-26 03:16:10 -0500

    Refactored http errors
    Signed-off-by: George Brotherston <George@StonePlayground.com>

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('_layouts.app')
+
+@section('content')
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-header" style="font-size: medium">
+						Error Page
+					</div>
+					<div class="card-body">
+						<p>
+							The page you attempted to reach was not found.&nbsp; Please navigate elsewhere.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
