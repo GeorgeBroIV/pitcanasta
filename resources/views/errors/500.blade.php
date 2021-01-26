67fac3a5872d80fa2ad1be77daf6f52dcc468d26
George Brotherston <George@StonePlayground.com>
2021-01-26 03:13:38 -0500

    Refactored http errors
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,20 @@
 @extends('_layouts.app')
 
 @section('content')
-	<div>
-		Error
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-header" style="font-size: medium">
+						Error Page
+					</div>
+					<div class="card-body">
+						<p>
+							An error has occurred, please navigate elsewhere.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 @endsection
